fb11a86f9ae14dae894ef7a7bff15ba042c720f9
tracer-NEMU2020 <tjuics2020@163.com>
2024-08-27 06:46:08 +0000

    >  compile NEMU 3022206045 codespace Linux codespaces-14de88 6.5.0-1022-azure #23~22.04.1-Ubuntu SMP Thu May  9 17:59:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  06:46:08 up  1:11,  0 users,  load average: 0.25, 0.32, 0.29 ffd1e7aedd3bacc925504d06fe581efece43db09

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -448,7 +448,7 @@ uint32_t eval(int p, int q)
 
         val1 = eval(p, op - 1);
         val2 = eval(op + 1, q);
-        printf("val1 = %d, val2 = %d\n", val1, val2);
+        // printf("val1 = %d, val2 = %d\n", val1, val2);
         switch (tokens[op].type)
         {
         case '+':

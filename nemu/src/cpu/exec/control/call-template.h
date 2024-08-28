8ad32d751583466b653ea5672e9914fb9af14443
tracer-NEMU2020 <tjuics2020@163.com>
2024-08-28 07:16:44 +0000

    >  compile NEMU 3022206045 codespace Linux codespaces-14de88 6.5.0-1022-azure #23~22.04.1-Ubuntu SMP Thu May  9 17:59:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  07:16:44 up 21 min,  0 users,  load average: 0.30, 0.36, 0.38 1f2c189184c6f595969f65925de7dfa862d8350c

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -1,49 +1,23 @@
-/*#include "cpu/exec/template-start.h"
-
-#define instr call
-
-make_helper(call_i_l){
-	 //根据SUFFIX进行译码，len为取出来的参数的字节数
-    int len = concat(decode_i_, SUFFIX)(cpu.eip + 1);
-    //堆栈操作
-    reg_l(R_ESP) -= 4;
-    swaddr_write(reg_l(R_ESP), 4, cpu.eip + len + 1);
-    //imm为偏移量的值
-    int imm = op_src -> val;
-    //打印指令
-    print_asm("call\t%x",cpu.eip + 1 + len + imm);
-    // 当前地址eip加上偏移量
-    cpu.eip += imm;
-    //返回该call指令总长度，最后会在外面再加上该值，正好满足指令：eip跳到下一条指令位置加上偏移量的位置
-    return len + 1;
-}
-
-#include "cpu/exec/template-end.h"
-*/
 #include "cpu/exec/template-start.h"
 
 #define instr call
 
 make_helper(concat(call_i_, SUFFIX)) {
-    //Push(EIP);
-    //EIP ← EIP + rel32;    
-    int len = concat(decode_i_, SUFFIX)(cpu.eip + 1);
-    reg_l(R_ESP) -= DATA_BYTE;
-    swaddr_write(reg_l(R_ESP), 4, cpu.eip + len + 1);
-    DATA_TYPE_S imm = op_src -> val;
-    print_asm("call %x", cpu.eip + 1 + len + imm);
-    cpu.eip = cpu.eip + imm;
-    return len + 1;
+	int len = concat(decode_i_, SUFFIX)(eip + 1); //calculate the length of instr (include opcode)
+	reg_l(R_ESP) -= DATA_BYTE; //esp - 4 (push)
+	MEM_W(reg_l(R_ESP), cpu.eip + len + 1); //write current eip next address to esp (mov)
+	cpu.eip += (DATA_TYPE_S)op_src->val; //length from eip to function
+	print_asm("call: 0x%x", cpu.eip + len + 1); // eip update
+	return len + 1; // return opcode + behind
 }
 
-make_helper(concat(call_rm_, SUFFIX)) { 
-    int len = concat(decode_rm_, SUFFIX)(cpu.eip + 1);
-    reg_l(R_ESP) -= DATA_BYTE;
-    swaddr_write(reg_l(R_ESP), 4, cpu.eip + len + 1);
-    DATA_TYPE_S imm = op_src -> val;
-    print_asm("call %x", imm);
-    cpu.eip = imm - len - 1;
-    return len + 1;
+make_helper(concat(call_rm_, SUFFIX)) {
+	int len = concat(decode_rm_, SUFFIX)(eip + 1);
+	reg_l(R_ESP) -= DATA_BYTE;
+	MEM_W(reg_l(R_ESP), cpu.eip + len + 1);
+	cpu.eip = (DATA_TYPE_S)op_src->val - len - 1;
+	print_asm("call: %s", op_src->str);
+	return len + 1;
 }
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/call.h
+++ nemu/src/cpu/exec/control/call.h
@@ -1,7 +1,5 @@
-#ifndef __CALL_H__
-#define __CALL_H__
+#include "cpu/exec/helper.h"
 
 make_helper(call_i_v);
-make_helper(call_rm_v);
 
-#endif
+make_helper(call_rm_v);

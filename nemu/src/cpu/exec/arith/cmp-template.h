e4147f328935cb08e599989236bc3524d193be75
tracer-NEMU2020 <tjuics2020@163.com>
2024-08-28 11:58:09 +0000

    >  compile NEMU 3022206045 root Linux codespaces-11f049 6.5.0-1022-azure #23~22.04.1-Ubuntu SMP Thu May  9 17:59:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:58:09 up  2:37,  0 users,  load average: 0.00, 0.12, 0.43 ba51d871afba20f9dbe1217444d6c7a3e697a0d4

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -2,30 +2,21 @@
 
 #define instr cmp
 
-static void do_execute() {
-    DATA_TYPE_S cha = (DATA_TYPE_S)(op_dest -> val) - (DATA_TYPE_S)(op_src -> val);
-    //printf("------------------------------------%x, %x, %x\n", op_src -> val, op_dest -> val, cha);
-    int len = (DATA_BYTE << 3) - 1;
-    //update_eflags_pf_zf_sf(cha);
-    cpu.eflags.ZF = !cha;
-    cpu.eflags.SF = cha >> len;
-	cpu.eflags.CF = op_dest -> val < op_src -> val;
-    int s1 = op_dest -> val >> len;
-	int s2 = op_src -> val >> len;
-	cpu.eflags.OF = (s1 != s2 && s2 == cpu.eflags.SF);
-    cha ^= cha >> 4;
-	cha ^= cha >> 2;
-	cha ^= cha >> 1;
-	cpu.eflags.PF = !(cha & 1);
-    print_asm_template2();
-}
+static void do_execute () {
+	DATA_TYPE result = op_dest->val - op_src->val;
 
-#if DATA_BYTE == 2 || DATA_BYTE == 4
-make_instr_helper(si2rm)
-#endif
+	update_eflags_pf_zf_sf((DATA_TYPE_S)result);
+	cpu.eflags.CF = result > op_dest->val;
+	cpu.eflags.OF = MSB((op_dest->val ^ op_src->val) & (op_dest->val ^ result));
+
+	print_asm_template2();
+}
 
 make_instr_helper(i2a)
 make_instr_helper(i2rm)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
 make_instr_helper(r2rm)
 make_instr_helper(rm2r)
 

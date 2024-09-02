c5c55ec6480f03785e44a8bd77dea64770107cdf
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-02 18:30:28 +0800

    >  run 3022206045 tommygong Linux TommyGong 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:30:28 up 2 min,  1 user,  load average: 0.40, 0.24, 0.10 cdaf0f4f364ee622a1fc118ede712509ff27db8

--- lib-common/FLOAT.h
+++ lib-common/FLOAT.h
@@ -22,4 +22,17 @@ static inline FLOAT F_div_int(FLOAT a, int b) {
 	return a / b;
 }
 
+static inline FLOAT F_div_F(FLOAT a, FLOAT b);
+
+static inline FLOAT pow(FLOAT x, FLOAT y);
+
+static inline FLOAT sqrt(FLOAT x);
+
+static inline FLOAT F_mul_F(FLOAT a, FLOAT b);
+
+static inline FLOAT f2F(float a);
+
+static inline FLOAT Fabs(FLOAT a);
+
+
 #endif

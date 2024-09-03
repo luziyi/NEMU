f9b3083670250e133abbad5eb8c0bd21e0522e1f
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-03 16:05:30 +0800

    >  run 3022206045 tommygong Linux TommyGong 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:05:30 up 9 min,  1 user,  load average: 0.11, 0.12, 0.09 35054eb9da937e7174665facae6e5ac11fa1c9c

--- lib-common/FLOAT.h
+++ lib-common/FLOAT.h
@@ -6,12 +6,11 @@
 typedef int FLOAT;
 
 static inline int F2int(FLOAT a) {
-	a &= 0xffff0000;
-	return a >> 16;
+	return (a >> 16);
 }
 
 static inline FLOAT int2F(int a) {
-	return a << 16;
+	return (a << 16);
 }
 
 static inline FLOAT F_mul_int(FLOAT a, int b) {
@@ -22,6 +21,16 @@ static inline FLOAT F_div_int(FLOAT a, int b) {
 	return a / b;
 }
 
+FLOAT f2F(float);
+FLOAT F_mul_F(FLOAT, FLOAT);
+FLOAT F_div_F(FLOAT, FLOAT);
+FLOAT Fabs(FLOAT);
+FLOAT sqrt(FLOAT);
+FLOAT pow(FLOAT, FLOAT);
 
+// used when calling printf/sprintf to format a FLOAT argument
+#define FLOAT_ARG(f) (long long)f
+
+void init_FLOAT_vfprintf(void);
 
 #endif
